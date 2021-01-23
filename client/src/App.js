0894482dbed6cddb122059b8fa329d6be7ccee75
HiepHop <hahahahahakhakhakha@gmail.com>
2021-01-23 13:38:30 -0800

    Commited at Heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Body from './Components/Body';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Body/>
 
       </div>
     );
--- client/src/Components/Body.js
+++ client/src/Components/Body.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import '../Assets/SASS/body.scss';
 
 class Body extends Component {
   render() {
     return (
       <div className="Body">
         <div className="body-container">
-          
+
         </div>
       </div>
     );
