b3e1df6b58bff76a76137eaad2750ec2f4959d2e
HiepHop <hahahahahakhakhakha@gmail.com>
2021-01-30 08:37:48 -0800

    Has Commited to frontend

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import '../Assets/SASS/navbar.scss';
 
 class Navbar extends Component {
+  capitalize = (str) =>{
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   render() {
+    const navList = ['home', 'project', 'about', 'contact'];
+    const navListMap = navList.map(navListMap => {
+      return <li><div><a href="/">{this.capitalize(`${navListMap}`)}</a></div></li>
+    });
 
     return (
-      <div className="Navbar">
-        
+      <div className="navbar-container">
+        <ul className="list-container">
+          {navListMap}
+        </ul>
       </div>
     );
   }
